b6ab4c76cece83a0b3a5ffc5ad6e3751c44a95e5
Andrea Pepe <57568233+AndreaPepe@users.noreply.github.com>
2021-03-11 09:52:45 +0100

    aggiunta progetto

---
+++ src/logic/HelloWorld.java
@@ -0,0 +1,10 @@
+package logic;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// print "Hello World!" to the standard output
+		System.out.println("Hello World!!!");
+	}
+
+}
---
+++ src/test/TestHello.java
@@ -0,0 +1,7 @@
+package test;
+
+public class TestHello {
+	public void test() {
+		System.out.println("test");
+	}
+}
